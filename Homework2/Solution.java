5c3a9f5734c0c419a6121679025e24b06de6894b
sashi <92218265+shacowka6on@users.noreply.github.com>
2024-03-16 16:57:57 +0200

    Update Solution.java

--- Homework2/Solution.java
+++ Homework2/Solution.java
@@ -45,6 +45,7 @@ class Solution{
             return IsDryer ? super.warranty() + (int)(0.5 * super.MinWarranty) : super.warranty();
         }
     }
+        /*------------------------------Method used for testing------------------------------*/
     public static void main(String[] args) {
         String manufacturerName = "ElDevMan";
         boolean isLongTermWarranty = false;
@@ -78,3 +79,44 @@ class Solution{
         System.out.println(electricDevice.warranty());
     }
 }
+/*------------------------------Method used in Hackerrank------------------------------
+     * public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String manufacturerName = bufferedReader.readLine();
+
+        boolean isLongTermWarranty = Integer.parseInt(bufferedReader.readLine().trim()) != 0;
+
+        int minWarranty = Integer.parseInt(bufferedReader.readLine().trim());
+
+        boolean isGas = Integer.parseInt(bufferedReader.readLine().trim()) != 0;
+
+        boolean isDryer = Integer.parseInt(bufferedReader.readLine().trim()) != 0;
+
+        // Create object of type Manufacturer using the constructor with two parameters. 
+        // Pass manufacturerName and isLongTermWarranty as arguments
+        Manufacturer manufacturer = new Manufacturer(manufacturerName, isLongTermWarranty);
+
+        // Create object of type ElectricDevice using the constructor with 2 parameters. 
+        // Pass manufacturer and minWarranty as arguments 
+        ElectricDevice electricDevice = new ElectricDevice(manufacturer, minWarranty);
+       
+        // Print on the console the warranty of the the object electricDevice, by calling warranty() method
+        System.out.println(electricDevice.warranty());
+
+        // Assign the electricDevice a new object of type Cooker using the constructor with 3 parameters. 
+        //Pass manufacturer, price and minWarranty as arguments 
+        electricDevice = new Cooker(manufacturer, minWarranty, isGas);
+
+        // Print on the console the warranty of the object electricDevice, by calling warranty() method
+        System.out.println(electricDevice.warranty());
+
+        // Assign the electricDevice a new object of type WashingMachine using the constructor with 3 parameters. 
+        // Pass manufacturer, price and minWarranty as arguments 
+        electricDevice = new WashingMachine(manufacturer, minWarranty, isDryer);
+
+        // Print on the console the warranty of the the object electricDevice, by calling warranty() method
+        System.out.println(electricDevice.warranty());
+        bufferedReader.close();
+    }
+     */
